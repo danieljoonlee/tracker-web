99ecf336c8ee5485a1034f8c4f360c953f238fa6
Daniel J Lee <danieljoonlee@gmail.com>
2016-06-17 09:06:22 -0700

    add initial state to replicate design, complete task to actions and reducers, and description to add tasks in actions

--- src/initialState.js
+++ src/initialState.js
@@ -4,12 +4,38 @@ const initialState = {
   projects: [
     {
       id: uuid(),
-      name: 'Sample Project',
+      name: 'Versus Todo List',
       tasks: {
-        count: 0,
-        todo: 0,
-        inProgress: 0,
-        list: []
+        count: 100,
+        todo: 40,
+        inProgress: 15,
+        complete: 45,
+        list: [{        
+          id: uuid(),
+          name: 'Task #1',
+          description: 'Description of Task #1',
+          state: 'to-do'},
+          {id: uuid(),
+          name: 'Task #2',
+          description: 'Description of Task #2',
+          state: 'to-do'},
+          {id: uuid(),
+          name: 'Task #3',
+          description: 'Description of Task #3',
+          state: 'to-do'},
+          {id: uuid(),
+          name: 'Task #1',
+          description: 'Description of Task #1',
+          state: 'in-progress'},
+          {id: uuid(),
+          name: 'Task #2',
+          description: 'Description of Task #2',
+          state: 'in-progress'},
+          { id: uuid(),
+          name: 'Task #3',
+          description: 'Description of Task #3',
+          state: 'in-progress'
+        }]
       }
     },
     {
@@ -19,6 +45,7 @@ const initialState = {
         count: 0,
         todo: 0,
         inProgress: 0,
+        complete: 0,
         list: []
       }
     }
